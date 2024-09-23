80b5cd5283fb9d8d1d25caa11dd13169460d210a
kelvin <kelvinmadridstar@gmail.com>
2024-09-23 18:05:45 +0000

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import NewStopwatch from "./NewStopwatch"
+import Stopwatch from "./Stopwatch"
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Stopwatch/>
+      <div style={{margin:40}}></div>
+      <NewStopwatch/>
+    </>
+  )
+}
+
+export default App
